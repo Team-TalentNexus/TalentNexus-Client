019b4be2ee0485cf527d635154b816f97c82da2b
wangpengju <207471063@qq.com>
2024-09-06 12:47:17 +0800

    添加简历

--- src/components/Test.vue
+++ src/components/Test.vue
@@ -52,7 +52,7 @@
       <el-form :model="form" ref="workExperienceForm" label-width="120px">
         <div v-for="(work, index) in form.workExperiences" :key="index" class="form-group">
           <el-form-item :label="'公司名称 ' + (index + 1)">
-            <el-input v-model="work.company"></el-input>
+            <el-input v-model="work.companyName"></el-input>
           </el-form-item>
           <el-form-item :label="'职位 ' + (index + 1)">
             <el-input v-model="work.position"></el-input>
@@ -66,6 +66,9 @@
           <el-form-item :label="'工作描述 ' + (index + 1)">
             <el-input type="textarea" v-model="work.description"></el-input>
           </el-form-item>
+          <el-form-item :label="'成就 ' + (index + 1)">
+            <el-input type="textarea" v-model="work.achievements"></el-input>
+          </el-form-item>
           <el-button type="danger" @click="removeWorkExperience(index)">删除</el-button>
           <el-divider></el-divider>
         </div>
@@ -75,8 +78,8 @@
 
     <!-- 底部按钮 -->
     <div class="deployBtn">
-      <el-button style="margin-top: 12px;" @click="prev" v-if="active === 1 || active === 2">上一步</el-button>
-      <el-button style="margin-top: 12px;" @click="next" v-if="active === 0 || active === 1">下一步</el-button>
+      <el-button style="margin-top: 12px;" @click="submitFormBasicInformation" v-if="active === 0">下一步</el-button>
+      <el-button style="margin-top: 12px;" @click="next" v-if="active === 1">下一步</el-button>
       <el-button style="margin-top: 12px;" @click="submitForm" v-if="active === 2">提交</el-button>
     </div>
   </el-card>
@@ -84,10 +87,12 @@
 
 <script>
 import request from '@/utils/request';
+import axios from 'axios';
 export default {
   data() {
     return {
       active: 0,
+      userToken: localStorage.getItem('token'),
       basicInformation: {
         title: '',
         summary: ''
@@ -105,11 +110,12 @@ export default {
         ],
         workExperiences: [
           {
-            company: '',
+            companyName: '',
             position: '',
             startDate: '',
             endDate: '',
-            description: ''
+            description: '',
+            achievements: ''
           }
         ]
       }
@@ -120,11 +126,6 @@ export default {
     next() {
       if (this.active < 2) {
         this.active++;
-      }
-    },
-    prev() {
-      if (this.active > 0) {
-        this.active--;
       }
     },
     addExperience() {
@@ -142,27 +143,83 @@ export default {
     },
     addWorkExperience() {
       this.form.workExperiences.push({
-        company: '',
+        companyName: '',
         position: '',
         startDate: '',
         endDate: '',
-        description: ''
+        description: '',
+        achievements: ''
       });
     },
     removeWorkExperience(index) {
       this.form.workExperiences.splice(index, 1);
     },
-    submitForm() {
-      // 这里可以根据需要验证和提交表单数据
-      this.$refs.workExperienceForm.validate((valid) => {
-        if (valid) {
-          alert('工作经历表单有效!');
-          // 提交逻辑
-        } else {
-          alert('表单无效，请检查输入.');
-          return false;
+    submitFormBasicInformation() {
+      if (this.active < 2) {
+        this.active++;
+      }
+      // 提交个人信息
+      try {
+        request.post('http://localhost:8081/seeker/resume', this.basicInformation, {
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${this.userToken}`
+          }
+        }).then(res => {
+          if (res.data.code === 200) {
+            this.$message.success('基本信息增加成功');
+          } else {
+            this.$message.error(res.data.message);
+          }
+        });
+      } catch (error) {
+        this.$message.error('提交基本信息时出错');
+      }
+
+    },
+    async submitForm() {
+      // 提交项目经历
+      for (const experience of this.form.projectExperiences) {
+        try {
+           request.post('http://localhost:8081/seeker/project', experience, {
+            headers: {
+              'Content-Type': 'application/json',
+              'Authorization': `Bearer ${this.userToken}`
+            }
+          }).then(res => {
+            if (res.data.code === 200) {
+              this.$message.success(`项目经历 "${experience.projectName}" 提交成功`);
+            } else {
+              this.$message.error(`项目经历 "${experience.projectName}" 提交失败: ${res.data.message}`);
+            }
+          });
+        } catch (error) {
+          this.$message.error(`提交项目经历 "${experience.projectName}" 时出错`);
         }
-      });
+      }
+
+      // 提交工作经历
+      for (const work of this.form.workExperiences) {
+        try {
+          request.post('http://localhost:8081/seeker/work', work, {
+            headers: {
+              'Content-Type': 'application/json',
+              'Authorization': `Bearer ${this.userToken}`
+            }
+          }).then(res => {
+            if (res.data.code === 200) {
+              this.$message.success(`工作经历 "${work.companyName}" 提交成功`);
+            } else {
+              console.log(res.data);
+              this.$message.error(`工作经历 "${work.companyName}" 提交失败: }`);
+            }
+          });
+        } catch (error) {
+          console.log(res.data.code);
+          console.log(error);
+          this.$message.error(`提交工作经历 "${work.companyName}" 时出错`);
+        }
+      }
     }
   }
 };
